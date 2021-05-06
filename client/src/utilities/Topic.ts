f6faa435faa1573ad500d7052d97582bd2174fe7
Matthew Bryant <matt23488@gmail.com>
2021-05-06 09:22:24 -0400

    topic improved typing

--- client/src/utilities/Topic.ts
+++ client/src/utilities/Topic.ts
@@ -6,23 +6,26 @@ interface TopicSubscription {
     callback: (data: any) => void;
 }
 const _subscriptionMap = new Map<string, TopicSubscription>();
-export const topic = {
-    publish: <T>(name: string, data?: T) => {
-        for (let subscription of _subscriptionMap.values()) {
-            if (subscription.name !== name) continue;
 
-            subscription.callback(data);
-        }
-    },
+function publish(name: string): void;
+function publish<T>(name: string, data: T): void;
+function publish<T>(name: string, data?: T) {
+    for (let subscription of _subscriptionMap.values()) {
+        if (subscription.name !== name) continue;
 
-    subscribe: <T>(name: string, callback: (data: T) => void) => {
-        const id = uuidv4();
-        const subscription = { id, name, callback };
-        _subscriptionMap.set(id, subscription);
-        return id;
-    },
-
-    unsubscribe: (topicId: string) => {
-        return _subscriptionMap.delete(topicId);
+        subscription.callback(data);
     }
-};
+}
+
+function subscribe<T>(name: string, callback: (data: T) => void) {
+    const id = uuidv4();
+    const subscription = { id, name, callback };
+    _subscriptionMap.set(id, subscription);
+    return id;
+}
+
+function unsubscribe(handle: string) {
+    return _subscriptionMap.delete(handle);
+}
+
+export const topic = { publish, subscribe, unsubscribe };
